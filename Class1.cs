8443a6becd444f8892279c2043a8bef5b6cbab7c
bthode <100352685+bthode@users.noreply.github.com>
2022-07-04 22:10:01 -0500

    Fizzbuzz boilerplate and tests

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_csharp
+{
+    class FizzBuzzer
+    {
+        public static void Main()
+        {
+
+        }
+
+
+      
+        public static string FizzBuzz(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FizzBuzz_csharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Fizz", FizzBuzzer.FizzBuzz(3));
+            Assert.AreEqual("Fizz", FizzBuzzer.FizzBuzz(6));
+            Assert.AreEqual("Fizz", FizzBuzzer.FizzBuzz(9));
+            Assert.AreEqual("Fizz", FizzBuzzer.FizzBuzz(18));
+
+            Assert.AreEqual("Buzz", FizzBuzzer.FizzBuzz(5));
+            Assert.AreEqual("Buzz", FizzBuzzer.FizzBuzz(10));
+            Assert.AreEqual("Buzz", FizzBuzzer.FizzBuzz(50));
+
+            Assert.AreEqual("FizzBuzz", FizzBuzzer.FizzBuzz(15));
+            Assert.AreEqual("FizzBuzz", FizzBuzzer.FizzBuzz(30));
+            Assert.AreEqual("FizzBuzz", FizzBuzzer.FizzBuzz(75));
+
+
+            Assert.AreEqual("2", FizzBuzzer.FizzBuzz(2));
+            Assert.AreEqual("4", FizzBuzzer.FizzBuzz(4));
+            Assert.AreEqual("88", FizzBuzzer.FizzBuzz(88));
+        }
+
+
+
+
+    }
+}
